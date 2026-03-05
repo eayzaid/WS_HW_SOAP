f05e3facfbb86f72287f75a50541dbe9b836b426
eayzaid <aymanzaidanedev@gmail.com>
2026-03-05 06:22:42 +0000

    initial setup , H2 database has been created , and the initlal XSD schema for the SOAP service is created

---
+++ src/main/java/com/example/student_soap/entities/Student.java
@@ -0,0 +1,5 @@
+package com.example.student_soap.entities;
+
+public class Student {
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO student (id, nom, email) VALUES (1, 'Hassan Alaoui', 'hassan.alaoui@email.ma');
+INSERT INTO student (id, nom, email) VALUES (2, 'Fatima Zahra', 'fatima.zahra@email.ma');
